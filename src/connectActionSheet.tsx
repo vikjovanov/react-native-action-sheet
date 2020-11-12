26f025be70bdbb197e14303281269d4a4a0fc7e6
Viktor Jovanovic <43797348+vikjovanov@users.noreply.github.com>
2020-11-12 16:34:11 +0100

    Update connectActionSheet.tsx
    add forwardedRef

--- src/connectActionSheet.tsx
+++ src/connectActionSheet.tsx
@@ -7,16 +7,21 @@ export default function connectActionSheet<OwnProps = any>(
   WrappedComponent: React.ComponentType<OwnProps & ActionSheetProps>
 ) {
   const ConnectedActionSheet = (props: OwnProps) => {
+    const { forwardedRef, ...rest } = props
     return (
       <Consumer>
         {({ showActionSheetWithOptions }) => {
           return (
-            <WrappedComponent {...props} showActionSheetWithOptions={showActionSheetWithOptions} />
+            <WrappedComponent ref={forwardedRef} {...rest} showActionSheetWithOptions={showActionSheetWithOptions} />
           );
         }}
       </Consumer>
     );
   };
+  
+  const ForwardedComponent = React.forwardRef((props, ref) => {
+    return <ConnectedActionSheet {...props} forwardedRef={ref} />;
+  })
 
-  return hoistNonReactStatic(ConnectedActionSheet, WrappedComponent);
+  return hoistNonReactStatic(ForwardedComponent, WrappedComponent);
 }
